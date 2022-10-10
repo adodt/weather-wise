820e4f2b6d28ffc787575f7c6ea3794b0c12cf41
Gator <allidodt@gmail.com>
2022-10-09 18:41:31 -0700

    initial JS. Added API

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,19 @@
 var current = moment();
-var searchBar = document.querySelector("input")
-var apiKey = "0c8dcf810ad08707d092e05436d4c4c0"
+var searchBar = document.querySelector("input");
+var apiKey = "0c8dcf810ad08707d092e05436d4c4c0";
+var apiUrl = "https://api.openweathermap.org";
 
+//Draws current day weather data
 
-
+function weatherToday (city, weather, time){
+    let date = moment().format('dddd');
+    let todayDate = document.getElementById("todayDate");
+    let todayIcon = document.getElementById("todayIcon");
+    let todayTemp = document.getElementById("todayTemp");
+    let todayWind = document.getElementById("todayWind");
+    let todayHumid = document.getElementById("todayHumid");
+    let weatherIcon = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+}
 
 
 
