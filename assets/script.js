7c831acd1d711cc39a1df8674a38995cbc892687
Gator <allidodt@gmail.com>
2022-10-09 18:05:31 -0700

    favicon element

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
-var moment = moment();
+var current = moment();
 var searchBar = document.querySelector("input")
+var apiKey = "0c8dcf810ad08707d092e05436d4c4c0"
 
 
 
 
 
 
-$("#currentDate").text(moment.format('MMMM Do YYYY, h:mm a'))
+//adds current date to header
+$("#currentDate").text(current.format('MMMM Do YYYY, h:mm a'))
+//runs getCity upon search click
 $("#searchButton").on("click", getCity)
